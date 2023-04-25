4de6253a0eaa5a2a605109c41a3f059ab6feddee
xotic <xoticc.ja@gmail.com>
2023-04-25 16:46:21 -0400

    yeahhhh

--- translate_py/script.py
+++ translate_py/script.py
@@ -1,3 +1,12 @@
 from translate import Translator
-translator = Translator(to_lang="jp")
-print(translation = translator.translate("My name is chris"))
+translator = Translator(to_lang="ja")
+#translation = translator.translate("My name is chris")
+#print(translation)
+try:
+    with open('text.txt', mode='r') as t:
+        text = t.read()
+        translation = translator.translate(text)
+        print(translation)
+except FileNotFoundError as e:
+    print('No file found')
+    
